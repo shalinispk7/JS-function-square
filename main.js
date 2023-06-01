20d0819dc89abf7323ca5bfe314c10fe6b4f7453
shalinispk7 <shalinispk7@gmail.com>
2023-06-01 20:51:58 +0530

    function square

---
+++ main.js
@@ -0,0 +1,15 @@
+// function-square
+// 1.square of the given number
+// let a=10;
+// function square(){
+	// result=a*a;
+	// document.write(result);
+// }
+// square();
+
+let a=parseInt(prompt("enter value"));
+function square(num){
+	result=num*num;
+	document.write("Square of the given number is "+(result));
+}
+square(a);
